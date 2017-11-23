bd30d5536d121f323d2eddb1dc1a78142e7806b8
161250080 <161250080@smail.nju.edu.cn>
2017-11-23 15:46:28 +0800

    no message

--- MSPSSERPnju/MSPSSClientModule/src/main/java/blimpl/promotionblimpl/PromotionBLInfo.java
+++ MSPSSERPnju/MSPSSClientModule/src/main/java/blimpl/promotionblimpl/PromotionBLInfo.java
@@ -1,4 +1,23 @@
 package blimpl.promotionblimpl;
 
+import po.PromotionPO;
+import util.Time;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Harper on 17/11/23
+ */
+
 public class PromotionBLInfo {
+    /**
+     * 搜索所有有效的促销策略
+     *
+     * @param date
+     * @return ArrayList<PromotionPO>
+     */
+    public ArrayList<PromotionPO> search(Time date){
+        return null;
+    }
+
 }
---
+++ MSPSSERPnju/MSPSSClientModule/src/test/java/promotion/promotionMockTest.java
@@ -0,0 +1,19 @@
+package promotion;
+
+import blimpl.promotionblimpl.MockPromotion;
+import blimpl.promotionblimpl.Promotion;
+import org.junit.Test;
+
+/**
+ * Created by Harper on 17/11/23
+ */
+public class promotionMockTest {
+    Promotion promotion = new MockPromotion();
+
+    @Test
+    public void teatAddPromotion(){
+        
+
+    }
+
+}
