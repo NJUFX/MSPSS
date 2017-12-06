b3ebcbddcdcd65aa9bcf1738f2a912737b4532f6
161250025 <161250025@smail.nju.edu.cn>
2017-12-06 17:20:29 +0800

    no message

--- MSPSSERPnju/MSPSSClientModule/src/main/java/ui/adminui/AdminMainViewController.java
+++ MSPSSERPnju/MSPSSClientModule/src/main/java/ui/adminui/AdminMainViewController.java
@@ -20,80 +20,83 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminMainViewController implements Initializable {
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
 
-    Dialog dialog = new Dialog();
-    private MainApp application;
+	Dialog dialog = new Dialog();
+	private MainApp application;
 
-    public void setApp(MainApp application) {
-        this.application = application;
-    }
+	public void setApp(MainApp application) {
+		this.application = application;
+	}
 
-    Stage stage = StageSingleton.getStage();
-    /*
-        @FXML
-        Button addUserButton;
-    */
-    @FXML
-    Button delUserButton;
+	Stage stage = StageSingleton.getStage();
 
-    @FXML
-    Button modUserButton;
+	@FXML
+	Button addUserButton;
 
+	@FXML
+	Button delUserButton;
 
-    public void addUserButtonAction(ActionEvent e) throws IOException {
-        try {
-            UserAddViewController controller = (UserAddViewController) replaceSceneContent("/view/admin/UserAddView.fxml");
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-    }
+	@FXML
+	Button modUserButton;
 
-    public void delUserButtonAction(ActionEvent e) throws IOException {
-        try {
-            UserDelViewController controller = (UserDelViewController) replaceSceneContent("/view/admin/UserDelView.fxml");
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-    }
+	public void addUserButtonAction(ActionEvent e) throws IOException {
+		try {
+			UserAddViewController controller = (UserAddViewController) replaceSceneContent(
+					"/view/admin/UserAddView.fxml");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
-    public void modUserButtonAction(ActionEvent e) throws IOException {
-        try {
-            UserModifyViewController controller = (UserModifyViewController) replaceSceneContent("/view/admin/UserModView.fxml");
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-    }
+	public void delUserButtonAction(ActionEvent e) throws IOException {
+		try {
+			UserDelViewController controller = (UserDelViewController) replaceSceneContent(
+					"/view/admin/UserDelView.fxml");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
-    /**
-     * 用来打开fxml文件
-     *
-     * @param fxml
-     * @return
-     * @throws Exception
-     */
-    private Initializable replaceSceneContent(String fxml) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        //InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(fxml);
-        InputStream in = MainApp.class.getResourceAsStream(fxml);
-        loader.setBuilderFactory(new JavaFXBuilderFactory());
-        loader.setLocation(MainApp.class.getResource(fxml));
-        Pane page;
-        try {
-            page = (Pane) loader.load(in);
-        } finally {
-            in.close();
-        }
-        Scene scene = new Scene(page, 900, 560);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        return (Initializable) loader.getController();
-    }
+	public void modUserButtonAction(ActionEvent e) throws IOException {
+		try {
+			UserModifyViewController controller = (UserModifyViewController) replaceSceneContent(
+					"/view/admin/UserModView.fxml");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * 用来打开fxml文件
+	 *
+	 * @param fxml
+	 * @return
+	 * @throws Exception
+	 */
+	private Initializable replaceSceneContent(String fxml) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		// InputStream in =
+		// Thread.currentThread().getContextClassLoader().getResourceAsStream(fxml);
+		InputStream in = MainApp.class.getResourceAsStream(fxml);
+		loader.setBuilderFactory(new JavaFXBuilderFactory());
+		loader.setLocation(MainApp.class.getResource(fxml));
+		Pane page;
+		try {
+			page = (Pane) loader.load(in);
+		} finally {
+			in.close();
+		}
+		Scene scene = new Scene(page, 900, 560);
+		stage.setScene(scene);
+		stage.sizeToScene();
+		return (Initializable) loader.getController();
+	}
 
 }
