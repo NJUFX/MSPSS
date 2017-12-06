ed18ce691a3a9382bb2f4e2f3ca2b74997e566bb
161250025 <161250025@smail.nju.edu.cn>
2017-12-06 22:02:52 +0800

    no message

--- MSPSSERPnju/MSPSSClientModule/src/main/java/ui/stocksellerui/CustomerManageViewController.java
+++ MSPSSERPnju/MSPSSClientModule/src/main/java/ui/stocksellerui/CustomerManageViewController.java
@@ -1,4 +1,14 @@
 package ui.stocksellerui;
 
-public class CustomerManageViewController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerManageViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //TODO
+    }
 }
