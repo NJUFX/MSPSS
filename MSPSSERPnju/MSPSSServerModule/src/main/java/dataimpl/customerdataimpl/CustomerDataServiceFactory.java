b41ed64f4f57eaed6dbb0a8b20bb98b1cdbc8f1f
161250164 <161250164@smail.nju.edu.cn>
2017-12-12 20:24:36 +0800

    add dataserviceimpl

--- MSPSSERPnju/MSPSSServerModule/src/main/java/dataimpl/customerdataimpl/CustomerDataServiceFactory.java
+++ MSPSSERPnju/MSPSSServerModule/src/main/java/dataimpl/customerdataimpl/CustomerDataServiceFactory.java
@@ -1,7 +1,18 @@
 package dataimpl.customerdataimpl;
 
+import datahelper.HibernateHelper;
+import po.CustomerPO;
+
 /**
  * Created by thinkpad on 2017/12/8.
  */
 public class CustomerDataServiceFactory {
+    private static CustomerDataServiceImpl customerDataService;
+
+    public static synchronized CustomerDataServiceImpl getCustomerDataService(){
+        if(customerDataService==null){
+            customerDataService = new CustomerDataServiceImpl(new HibernateHelper<CustomerPO>());
+        }
+        return customerDataService;
+    }
 }
--- MSPSSERPnju/MSPSSServerModule/src/main/java/dataservice/CustomerDataService.java
+++ MSPSSERPnju/MSPSSServerModule/src/main/java/dataservice/CustomerDataService.java
@@ -2,65 +2,63 @@ package dataservice;
 
 import po.*;
 import util.*;
+
+import java.util.ArrayList;
+
 public interface CustomerDataService {
 
     /**
-     * 通过客户编号获取客户属性
-     *
+     * 增加客户
+     * @param po
+     * @return
+     */
+    public ResultMessage addCustomer(CustomerPO po);
+
+    /**
+     * 更新
+     * @param po
+     * @return
+     */
+    public ResultMessage updateCustomer(CustomerPO po);
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    public ResultMessage deleteCustomer(String id);
+
+    /**
+     * 完全匹配
+     * @param field
+     * @param value
+     * @return
+     */
+    public ArrayList<CustomerPO> fullSearchCustomer(String field, Object value);
+
+    /**
+     * 模糊查询
+     * @param field
+     * @param value
+     * @return
+     */
+    public ArrayList<CustomerPO> fuzzSearchCustomer(String field,String value);
+
+    /**
+     * 范围查找
+     * @param filed
+     * @param min
+     * @param max
+     * @return
+     */
+    public ArrayList<CustomerPO> rangeSearchCustomer(String filed,Object min,Object max);
+
+    /**
+     * 精确搜索id
      * @param ID
      * @return
      */
-    public CustomerPO getCustomerInfo(String ID);
-
-    /**
-     * 增加客户
-     *
-     * @param ID       客户编号
-     * @param category 分类
-     * @param level
-     * @param name
-     * @param tele
-     * @param address
-     * @param postcode 邮编
-     * @param email
-     * @param InValue  应收额度
-     * @param in       应收
-     * @param out      应付
-     * @param DAE      默认业务员
-     * @return
-     */
-    public boolean addCustomer(String ID, String category, int level, String name, String tele, String address, String postcode, String email, double InValue, double in, double out, String DAE);
-
-    /**
-     * 增加客户
-     *
-     * @param customer
-     * @return
-     */
-    public boolean addCustomer(CustomerPO customer);
-
-    /**
-     * 删除客户
-     *
-     * @param ID
-     * @return
-     */
-    public boolean delCustomer(String ID);
-
-    /**
-     * 修改客户属性
-     *
-     * @param category
-     * @param level
-     * @param name
-     * @param tele
-     * @param address
-     * @param postcode
-     * @param email
-     * @param InValue
-     * @return
-     */
-    public boolean modifyCustomer(String ID, String category, int level, String name, String tele, String address, String postcode, String email, double InValue);
+    public CustomerPO searchCustomerByID(String ID);
 
 
 }
