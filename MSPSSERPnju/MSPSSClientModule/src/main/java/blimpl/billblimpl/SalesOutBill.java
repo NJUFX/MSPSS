725cb0ddce5f43a5c9407aede74335da86cbc8e2
161250036 <hanxinhu521@163.com>
2017-12-28 12:00:49 +0800

    完成了大部分销售单的操作，还有赠品操作尚未实现

--- MSPSSERPnju/MSPSSClientModule/src/main/java/blimpl/billblimpl/SalesOutBill.java
+++ MSPSSERPnju/MSPSSClientModule/src/main/java/blimpl/billblimpl/SalesOutBill.java
@@ -1,15 +1,31 @@
 package blimpl.billblimpl;
 
+import blimpl.blfactory.BLFactoryImpl;
+import blservice.commodityblservice.CommodityInfoService;
+import blservice.customerblservice.CustomerBLInfo;
+import blservice.userblservice.UserInfo;
+import network.BillClientNetworkService;
+import po.SalesItemPO;
+import po.SalesOutBillPO;
+import util.BillStatus;
 import util.ResultMessage;
-import vo.SalesOutBillVO;
+import util.SalesOutBillType;
+import util.Time;
+import vo.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description:
  * Created by Hanxinhu at 13:23 2017/11/21/021
  */
 public class SalesOutBill {
+    private static BillClientNetworkService networkService;
+    private static CommodityInfoService commodityinfoService = new BLFactoryImpl().getCommodityInfoService();
+    private static UserInfo userInfo = new BLFactoryImpl().getUserInfo();
+    private static CustomerBLInfo customerBLInfo = new BLFactoryImpl().getCustomerBLInfo();
+
     /**
      * 添加销售单
      * 保存
@@ -18,7 +34,16 @@ public class SalesOutBill {
      * @return
      */
     public ResultMessage saveSalesOutBill(SalesOutBillVO vo) {
-        return ResultMessage.SUCCESS;
+        vo.setStatus(BillStatus.init);
+        if (vo.getInit_time() == null) {
+            vo.setInit_time(new Time());
+        }
+        if (vo.getID() == null) {
+            String id = networkService.getSalesOutBillID(vo.getType());
+            vo.setID(id);
+            return networkService.addSalesOutBill(vo_to_po(vo));
+        }
+        return networkService.updateSalesOutBill(vo_to_po(vo));
     }
 
     ;
@@ -30,10 +55,14 @@ public class SalesOutBill {
      * @return
      */
     public ResultMessage commitSalesOutBill(SalesOutBillVO vo) {
-        return ResultMessage.SUCCESS;
+        vo.setCommit_time(new Time());
+        vo.setStatus(BillStatus.commit);
+        ArrayList list = vo.getPresentations();
+        //fixme 这里应该有一个调用接口直接产生库存赠送单 待定
+        return networkService.updateSalesOutBill(vo_to_po(vo));
     }
 
-    ;
+
 
     /**
      * 删除销售单
@@ -42,10 +71,12 @@ public class SalesOutBill {
      * @return
      */
     public ResultMessage deleteSalesOutBill(SalesOutBillVO vo) {
-        return ResultMessage.SUCCESS;
+        if (vo.getStatus() == BillStatus.init)
+            return networkService.deleteSalesOutBill(vo.getID());
+        return ResultMessage.FAILED;
     }
 
-    ;
+
 
     /**
      * 得到自己的销售单
@@ -54,29 +85,99 @@ public class SalesOutBill {
      * @return
      */
     public ArrayList<SalesOutBillVO> getMySalesOutBill(String operatorID) {
-        return new ArrayList<>();
+        List<SalesOutBillPO> pos = networkService.fullSearchSalesOutBill("operatorID", operatorID);
+        return pos_to_vos(pos);
     }
 
-    ;
+
 
     /**
      * @param vo
      * @return
      */
     public ResultMessage withdrawSalesOutBill(SalesOutBillVO vo) {
-        return ResultMessage.SUCCESS;
+        vo.setStatus(BillStatus.init);
+        vo.setCommit_time(null);
+        return networkService.updateSalesOutBill(vo_to_po(vo));
     }
 
     public ArrayList<SalesOutBillVO> getWaitingSalesOutBill() {
-        return new ArrayList<>();
+        List<SalesOutBillPO> pos = networkService.fullSearchSalesOutBill("status", BillStatus.commit.ordinal());
+
+        return pos_to_vos(pos);
     }
 
     public ResultMessage approveSalesOutBill(SalesOutBillVO salesOutBillVO) {
-        return ResultMessage.SUCCESS;
+        salesOutBillVO.setStatus(BillStatus.approval);
+        salesOutBillVO.setApproval_time(new Time());
+        return networkService.updateSalesOutBill(vo_to_po(salesOutBillVO));
     }
 
     public ResultMessage rejectSalesOutBill(SalesOutBillVO salesOutBillVO) {
-        return ResultMessage.SUCCESS;
+        salesOutBillVO.setApproval_time(new Time());
+        salesOutBillVO.setStatus(BillStatus.rejected);
+        return networkService.updateSalesOutBill(vo_to_po(salesOutBillVO));
     }
 
+    public static SalesOutBillVO po_to_vo(SalesOutBillPO po) {
+        Time initTime = po.getInit_time() != null ? new Time(po.getInit_time()) : null;
+        Time commitTime = po.getInit_time() != null ? new Time(po.getCommit_time()) : null;
+        Time approvalTime = po.getInit_time() != null ? new Time(po.getApproval_time()) : null;
+        UserVO operator = po.getOperatorID() != null ? userInfo.getUser(po.getOperatorID()) : null;
+        UserVO manager = po.getManagerID() != null ? userInfo.getUser(po.getManagerID()) : null;
+        SalesOutBillType type = SalesOutBillType.values()[po.getType()];
+        BillStatus status = BillStatus.values()[po.getStatus()];
+        CustomerVO customerVO = customerBLInfo.getCustomerByID(po.getCustomerID());
+        List<SalesItemPO> salesItemPOS = po.getSalesItemPOS();
+        ArrayList<SalesItemVO> salesItemVOS = new ArrayList<>();
+        for (SalesItemPO itemPO :
+                salesItemPOS) {
+            salesItemVOS.add(po_to_vo(itemPO));
+        }
+
+        SalesOutBillVO vo = new SalesOutBillVO(po.getID(), type, status, customerVO, po.getDAE(), po.getStorage()
+                , null, salesItemVOS, null, po.getAllowance(), po.getVoucher(), po.getPresent_vocher(), initTime
+                , commitTime, approvalTime, manager, operator, po.getSumBeforeDiscount(), po.getSumAfterDiscount());
+        return vo;
+    }
+
+    public static SalesOutBillPO vo_to_po(SalesOutBillVO vo) {
+        String initTime = vo.getInit_time() != null ? vo.getInit_time().toString() : null;
+        String commitTime = vo.getCommit_time() != null ? vo.getCommit_time().toString() : null;
+        String approvalTime = vo.getApproval_time() != null ? vo.getApproval_time().toString() : null;
+        String operatorID = vo.getOperator() != null ? vo.getOperator().getID() : null;
+        String managerID = vo.getManager() != null ? vo.getManager().getID() : null;
+        String customerID = vo.getCustomerVO() != null ? vo.getCustomerVO().getID() : null;
+
+        ArrayList<SalesItemPO> itemPOS = new ArrayList<>();
+
+        for (SalesItemVO itemVO : vo.getItemVOS()
+                ) {
+            itemPOS.add(vo_to_po(itemVO));
+        }
+        SalesOutBillPO po = new SalesOutBillPO(vo.getID(), vo.getType().ordinal(), customerID,
+                vo.getDAE(), operatorID, managerID, vo.getStatus().ordinal(), vo.getStorage(), initTime, commitTime, approvalTime,
+                vo.getPs(), vo.getAllowance(), vo.getVoucher(), vo.getPresentation_voucher(),
+                vo.getSumBeforeDiscount(), vo.getSumAfterDiscount(), itemPOS);
+        return po;
+    }
+
+    public static ArrayList<SalesOutBillVO> pos_to_vos(List<SalesOutBillPO> pos) {
+        ArrayList<SalesOutBillVO> salesOutBillVOS = new ArrayList<>();
+        for (SalesOutBillPO po :
+                pos) {
+            salesOutBillVOS.add(po_to_vo(po));
+        }
+        return salesOutBillVOS;
+    }
+
+    public static SalesItemPO vo_to_po(SalesItemVO vo) {
+        return new SalesItemPO(vo.getId(), vo.getNumber(), vo.getPrice(), vo.getPs());
+    }
+
+    public static SalesItemVO po_to_vo(SalesItemPO po) {
+        CommodityVO commodityVO = commodityinfoService.getCommodity(po.getId());
+        SalesItemVO vo = new SalesItemVO(commodityVO, po.getNumber(), po.getPrice());
+        return vo;
+    }
 }
