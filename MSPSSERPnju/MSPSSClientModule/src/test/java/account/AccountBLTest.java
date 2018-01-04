14725dcaf62c117da0a8445eb73fa7258bf2cb6c
161250036 <hanxinhu521@163.com>
2018-01-04 08:51:56 +0800

    debug

--- MSPSSERPnju/MSPSSClientModule/src/test/java/account/AccountBLTest.java
+++ MSPSSERPnju/MSPSSClientModule/src/test/java/account/AccountBLTest.java
@@ -1,9 +1,26 @@
 package account;
 
+import blimpl.blfactory.BLFactoryImpl;
+import blservice.accountblservice.AccountBLService;
+import org.junit.Test;
+import vo.AccountFilterFlagsVO;
+import vo.AccountVO;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Description:
  * Created by Hanxinhu at 11:35 2017/12/31/031
  */
 public class AccountBLTest {
+    AccountBLService service = new BLFactoryImpl().getAccountBLService();
 
+    @Test
+    public void test1() {
+        AccountFilterFlagsVO accoutFilterFlagVO = new AccountFilterFlagsVO("12", null, null);
+        ArrayList<AccountVO> list = service.searchAccount(accoutFilterFlagVO);
+        assertEquals(1, list.size());
+    }
 }
