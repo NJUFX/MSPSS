ddee38ebb646a83d4dbc2dba73887eb7575b0fa2
161250080 <161250080@smail.nju.edu.cn>
2018-01-12 22:36:14 +0800

    修复玄学bug

--- MSPSSERPnju/MSPSSCommonModule/src/main/java/util/Encryptor.java
+++ MSPSSERPnju/MSPSSCommonModule/src/main/java/util/Encryptor.java
@@ -45,7 +45,7 @@ public class Encryptor {
     }
 
     public static void main(String[] args) {
-        System.out.println(Encryptor.encrypt("980626h"));
+        System.out.println(Encryptor.encrypt("wsxgy813"));
         System.out.println(Encryptor.encrypt("980626ha"));
         System.out.println(Encryptor.encrypt("980626han"));
     }
